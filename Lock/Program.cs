0618e641406009896d0de56652f61534c761ab5f
Nour Nafea <63984872+NourNafea@users.noreply.github.com>
2024-04-17 13:33:36 +0200

    add lock

--- Lock/Program.cs
+++ Lock/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿for (var i = 0; i < 5; i++)
+{
+    new Thread(DoWork).Start();
+}
 
-Console.WriteLine("Hello, World!");
+Console.ReadLine();
+return;
+
+static void DoWork()
+{
+    lock (typeof(Program))
+    {
+        Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is working.");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is done.");
+    }
+}
