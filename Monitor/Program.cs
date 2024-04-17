c22e37870a0987b14045f3ad0c865224f94609f7
Nour Nafea <63984872+NourNafea@users.noreply.github.com>
2024-04-17 15:59:26 +0200

    add monitor

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿for (var i = 0; i < 5; i++)
+{
+    new Thread(DoWork).Start();
+}
 
-Console.WriteLine("Hello, World!");
+Console.ReadLine();
+return;
+
+static void DoWork()
+{
+    try
+    {
+        Monitor.Enter(typeof(Program));
+        Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is working.");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is done.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+    finally
+    {
+        Monitor.Exit(typeof(Program));
+    }
+}
