35904e8633fdc5ce0b62caa5f0557ecd236a5bc9
Nour Nafea <63984872+NourNafea@users.noreply.github.com>
2024-04-17 17:21:23 +0200

    add mutex

--- Mutex/Program.cs
+++ Mutex/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var mutex = new Mutex();
 
-Console.WriteLine("Hello, World!");
+for (var i = 0; i < 5; i++)
+{
+    new Thread(Write).Start();
+}
+Console.ReadLine();
+return;
+
+void Write()
+{
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is waiting for Writing.");
+    mutex.WaitOne();
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is writing.");
+    Thread.Sleep(5000);
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is done writing.");
+    mutex.ReleaseMutex();
+}
