4422990fe07b8f53a37e3b439c5f3d96f7ce277c
Nour Nafea <63984872+NourNafea@users.noreply.github.com>
2024-04-17 17:12:49 +0200

    add auto reset event

---
+++ AutoResetEvent/Program.cs
@@ -0,0 +1,21 @@
+﻿var autoResetEvent = new AutoResetEvent(false);
+
+for (var i = 0; i < 5; i++)
+{
+    new Thread(Write).Start();
+}
+
+Thread.Sleep(3000);
+autoResetEvent.Set();
+Console.ReadLine();
+return;
+
+void Write()
+{
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is waiting for Writing.");
+    autoResetEvent.WaitOne();
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is writing.");
+    Thread.Sleep(5000);
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is done writing.");
+    autoResetEvent.Set();
+}
