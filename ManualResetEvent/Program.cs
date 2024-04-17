589c36d742dd8018ca892a5517f18171a24b13bd
Nour Nafea <63984872+NourNafea@users.noreply.github.com>
2024-04-17 16:27:14 +0200

    add manual reset event

---
+++ ManualResetEvent/Program.cs
@@ -0,0 +1,27 @@
+﻿var manualResetEvent = new ManualResetEvent(false);
+
+new Thread(Write).Start();
+
+for (var i = 0; i < 5; i++)
+{
+    new Thread(Read).Start();
+}
+
+Console.ReadLine();
+return;
+
+void Write()
+{
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is waiting for Writing.");
+    manualResetEvent.Reset();
+    Thread.Sleep(5000);
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is done writing.");
+    manualResetEvent.Set();
+}
+
+void Read()
+{
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is waiting for reading.");
+    manualResetEvent.WaitOne();
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is done reading.");
+}
